cba5ffb64d80e15d5c513bfc3f7f45dc6e6eb50e
ntcrckr <alexei.pozdnyakov@mail.ru>
2023-01-30 20:14:06 +0300

    stuff to show all users
    changed some layouts back
    added list of all users to root page
    overwritten devise log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
-  # resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
